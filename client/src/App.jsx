ac524232232ef21349f5266cd71356ce162900c8
Shifa200 <shifa20naqvi@gmal.com>
2025-07-15 11:21:08 +0530

    project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+        <div className="min-h-screen bg-yellow-100 flex items-center justify-center">
+      <h1 className="text-4xl text-blue-700 font-bold">
+        Tailwind is working! 🎉
+        </h1>
+    </div>
+  );
+}
+
+export default App;
